fab88bf50a030978793b50dd54d885eb5692c6cf
kohei <koheimtri3@gmail.com>
2025-04-06 23:25:44 +0900

    use min function

--- sample/modernize_sample.go
+++ sample/modernize_sample.go
@@ -6,12 +6,8 @@ func main() {
 	a := 10
 	b := 20
 
-	var minVal int
-	if a < b {
-		minVal = a
-	} else {
-		minVal = b
-	}
+	// Go 1.21 以降では、min 関数が組み込みとして利用可能
+	minVal := min(a, b)
 
 	fmt.Println("Minimum:", minVal)
 }
